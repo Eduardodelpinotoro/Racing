4796e01352713b10850e082da146fef1d9f08755
Edu <delpinotoro@gmail.com>
2025-11-17 11:00:18 +0100

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'spa-game-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdn.babylonjs.com/babylon.js',
+  'https://cdn.babylonjs.com/loaders/babylon.glTF2FileLoader.min.js',
+  './spa.glb'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
